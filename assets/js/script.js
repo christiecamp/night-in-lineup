426c292b8779fdd6c352248bb7bfb152e3d48bfe
lilcobby <jshairfield13@gmail.com>
2023-09-28 20:48:58 -0400

    revert to base code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,1 @@
 console.log("hello");
-console.log("this");
-console.log("this");
-console.log("this");
-console.log("this");
-console.log("this");
-console.log("this");
-console.log("this");
-console.log("this");
-console.log("this");
-console.log("is a test");
-
-
-
-
-this is some junk
